38e5026515aef54171d3f5d1759023a3272f9ef6
tubarahat <201446223+tubarahat@users.noreply.github.com>
2026-01-31 20:13:32 +0530

    Implement middleware and define new API routes
    Added middleware for logging and rate limiting, and defined routes for users, vehicles, and trips.

--- index.js
+++ index.js
@@ -33,3 +33,25 @@ app.use('/api/analytics', require('./routes/analytics.routes'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Fleet API running on port ${PORT}`));
+
+
+const express = require('express');
+const app = express();
+const logger = require('./middlewares/logger.middleware');
+const vehicleLimiter = require('./middlewares/rateLimiter.middleware');
+
+app.use(express.json());
+app.use(logger); // Apply Logger globally
+
+// Routes
+app.use('/api/users', require('./routes/user.routes'));
+app.post('/api/vehicles/add', vehicleLimiter, require('./controllers/vehicle.controller').addVehicle); // Limiter applied here
+app.use('/api/trips', require('./routes/trip.routes'));
+app.get('/analytics', require('./controllers/analytics.controller').getAnalytics);
+
+// Handling Undefined Route Middleware
+app.use((req, res) => {
+    res.status(404).json({ message: "This Request Is Not Found" });
+});
+
+app.listen(3000, () => console.log("System Online"));
