226b32eefc913e05e70b8684ec8115d57914f552
tubarahat <201446223+tubarahat@users.noreply.github.com>
2026-01-31 19:27:10 +0530

    Add user signup route

---
+++ user.routes.js
@@ -0,0 +1,9 @@
+// routes/user.routes.js
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/user.controller');
+
+router.post('/signup', userController.signUp);
+
+module.exports = router;
+
